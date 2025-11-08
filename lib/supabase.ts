d24aa0b7b1eb28362bc3b6ca708ae22cdccdacd0
alialali222726-creator <alialali222726@gmail.com>
2025-11-08 17:41:06 +0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.https://bllhyrcaovvbtzbwppav.supabase.co;
+const supabaseUrl = process.env.https//bllhyrcaovvbtzbwppav.supabase.co;
 const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsbGh5cmNhb3Z2YnR6YndwcGF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2MDkzODIsImV4cCI6MjA3ODE4NTM4Mn0.MMRgp1uNSBbF8IT1LanI2G8wkavqp4BxLFzOTS8fyDg;
 
 if (!supabaseUrl || !supabaseAnonKey) {
